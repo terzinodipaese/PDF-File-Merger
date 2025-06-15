d3e9d5ca1a2196eee3d6d443211f6ab578be0587
Scivoletto Mattia Maria <30941518+terzinodipaese@users.noreply.github.com>
2025-06-15 08:54:11 +0200

    Update pdf_merger.py

--- pdf_merger.py
+++ pdf_merger.py
@@ -19,7 +19,7 @@ if isHelperOptionPresent == True:
     print("Usage: .\\pdf_merger [--help]")
     print("       .\\pdf_merger --list file1.pdf file2.pdf [file3.pdf ...] --output output.pdf")
     print("> Specify the files to merge with the full path, not just the filename.")
-    print("> Use double quotes if full path contains empty spaces.")
+    print("> Use double or single quotes if full path contains empty spaces.")
     print("> Specify just a file name after --output option. If you specify more than one, only the first one will be used.")
     exit()
 
